fc01c62a3d3177e3f756f7bfdc004cdd88912704
Andreyglass1989 <1989andreyglass@gmail.com>
2019-02-19 15:29:19 +0200

    7. M2M Changed Signal to Calculate Cart Total

--- carts/models.py
+++ carts/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 from Store.models import Product
+from django.db.models.signals import pre_save, post_save, m2m_changed
 
 # Create your models here.
 
@@ -37,6 +38,7 @@ class CartManager(models.Manager):
 class Cart(models.Model):
 	user = models.ForeignKey(User, null=True, blank=True)
 	products = models.ManyToManyField(Product, blank=True)
+	subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
 	total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
@@ -45,3 +47,28 @@ class Cart(models.Model):
 
 	def __str__(self):
 		return str(self.id)
+
+
+
+def m2m_change_cart_receiver(sender, instance, action, *args, **kwargs):
+	if action == "post_add" or action == "post_remove" or action == "post_clear":
+		# print(instance.total)
+		# print(instance.products.all())
+		products = instance.products.all()
+		total = 0
+		for x in products:
+			total += x.price
+		if instance.subtotal != total:
+
+			# print(total)
+			instance.subtotal = total
+			instance.save()
+
+
+m2m_changed.connect(m2m_change_cart_receiver, sender=Cart.products.through)
+
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+	instance.total = instance.subtotal + 10
+
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
